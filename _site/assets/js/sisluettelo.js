b1c47ee0eacb0f8e615a9636e86742f594926d1b
KimmoCommit <registerstuffhere@gmail.com>
2014-05-06 18:56:42 +0300

    lisätty kansiokuvakkeet kansioille ja tiedostoille oma kuvake, joiden väri muutettu perusmustasta harmaaksi

--- _site/assets/js/sisluettelo.js
+++ _site/assets/js/sisluettelo.js
@@ -10,7 +10,7 @@ $(function(){
 								var kansioKategoria = $(this).children(".alikansio-kategoria").text().trim();
 
 								$(this).attr("id",thisTrimmerText);
-								$(".kansio-puu-ul").append('<a href="#' + thisTrimmerText + '" data-category="' + kansioKategoria + '" class="' + Class + ' alikansio-puu-linkki ' + kansioKategoria + '"><li><div class="kansio-puu-merkki kansio-puu-alimerkki">'+thisText+'</div></li></a>'); 
+								$(".kansio-puu-ul").append('<a href="#' + thisTrimmerText + '" data-category="' + kansioKategoria + '" class="' + Class + ' alikansio-puu-linkki ' + kansioKategoria + '"><li><div class="kansio-puu-alimerkki kansio-puu-merkki"><img style="float:left;" src="assets/img/little7.svg" />'+thisText+'</div></li></a>'); 
 							}
 							//if-lause on tässä välttämätön!
 							else if($(this).children(".kansio-otsikko").text().length > 0){
@@ -18,7 +18,7 @@ $(function(){
 								var thisTrimmerText = $(this).children(".kansio-otsikko").text().replace(/ /g, '').trim();
 								var Class="kansio-puu-linkki";
 								$(this).attr("id",thisTrimmerText);
-								$(".kansio-puu-ul").append('<a href="#' + thisTrimmerText + '" data-category="' + thisTrimmerText + '" class="' + Class + ' ylikansio"><li><div class="kansio-puu-merkki">'+thisText+'</div></li></a>'); 
+								$(".kansio-puu-ul").append('<a href="#' + thisTrimmerText + '" data-category="' + thisTrimmerText + '" class="' + Class + ' ylikansio"><li><div class="kansio-puu-ylimerkki kansio-puu-merkki"><img style="float:left;" src="assets/img/little7.svg" />'+thisText+'</div></li></a>'); 
 							}
 							else {
 								return;	
